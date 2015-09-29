36827f76482e3ef3162ed81ed77c74731e7ce161
Pavel Moravec <pavelm@salsitasoft.com>
2015-09-29 14:39:52 +0200

    query proxy working

--- src/server/api-proxy.js
+++ src/server/api-proxy.js
@@ -11,32 +11,32 @@ app.get('/search', function (req, res) {
   res.header('Access-Control-Allow-Origin', 'http://' + config.domain + ':' + config.port);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
-  var query = req.query.query || '';
+  var userQuery = req.query.query || '';
+
+  console.log(userQuery);
 
   // If query not defined return 400
-  if (!query) {
+  if (!userQuery) {
     return res.status(400).send('Bad Request');
-  }
-
-  request({
-    url: 'https://query.yahooapis.com/v1/public/yql',
-    qs: {
-      q: 'select * from flickr.photos.interestingness(20) where api_key=' + config.api_key,
-      format: 'json'
-    },
-    json: true
-  }, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
+  } else {
+    request({
+      url: 'https://query.yahooapis.com/v1/public/yql',
+      qs: {
+        q: 'select * from flickr.photos.search where has_geo="true" and text="' + userQuery + '" and api_key="' + config.api_key+ '"',
+        format: 'json'
+      },
+      json: true
+    }, function (error, response, body) {
       var photos = body.query.count > 1
         ? body.query.results.photo
         : body.query.count
           ? [body.query.results.photo]
           : [];
-      return photos;
-    } else {
-      return error;
-    }
-  });
+      console.log('error: ' + error);
+      console.log('photos: ' + photos);
+      return res.send(photos);
+    });
+  }
 });
 
 app.listen(config.port, config.domain, function(err) {

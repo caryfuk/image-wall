fd140874a31f0e2fc7835ac43198d2d429c1eaaa
Pavel Moravec <pavelm@salsitasoft.com>
2015-09-30 00:04:51 +0200

    results rendering - WIP

--- src/client/App.js
+++ src/client/App.js
@@ -3,24 +3,51 @@ import Request from 'request';
 
 export default class App extends Component {
 
-  getInitialState() {
-    return {
-      photos: {}
+  constructor() {
+    super();
+
+    this.state = {
+      photos: []
     };
   }
 
-  getPhotos() {
+  componentDidMount() {
+    var self = this;
     Request('http://localhost:3001/search?query=Hloubětín', function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        this.setState({photos: body.response});
+        self.setState({photos: JSON.parse(body)});
       }
-    })
+    });
+  }
+
+  renderResults() {
+    if (this.state.photos.length > 0) {
+      this.state.photos.map(function (photo, index) {
+        var imageUrl = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg';
+
+        return (
+          <li>
+            <div>
+              <a href={imageUrl} target="_blank">
+                <img src={imageUrl} alt={photo.title} />
+                <span>{photo.title}</span>
+              </a>
+            </div>
+          </li>
+        );
+      });
+    } else {
+      return <p>empty</p>;
+    }
   }
 
   render() {
     return (
       <div>
-        <h1>Hello world</h1>
+        <h1>Image wall</h1>
+        <ul>
+          {this.renderResults()}
+        </ul>
       </div>
     );
   }

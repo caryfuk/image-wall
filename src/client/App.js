95d680c2858a7bf283d8fe073e8520b4f3e4215e
Pavel Moravec <pavelm@salsitasoft.com>
2015-09-30 16:37:08 +0200

    now using request-promise instead of request

--- src/client/App.js
+++ src/client/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Request from 'request';
+import rp from 'request-promise';
 
 export default class App extends Component {
 
@@ -13,11 +13,15 @@ export default class App extends Component {
 
   componentDidMount() {
     var self = this;
-    Request('http://localhost:3001/search?query=Hloubětín', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        self.setState({photos: JSON.parse(body)});
-      }
-    });
+    rp('http://localhost:3001/search?query=Hloubětín')
+      .then(
+        function(response) {
+          self.setState({photos: JSON.parse(response)});
+        }
+      )
+      .catch(function(e) {
+        console.error('Error loading response.');
+      });
   }
 
   renderResults() {
@@ -26,7 +30,6 @@ export default class App extends Component {
         <ul>
           {this.state.photos.map(function (photo, index) {
             var imageUrl = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg';
-
             return (
               <li>
                 <div>

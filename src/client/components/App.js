2a6fef14a2137db7558be3559d0694c53ec62f7d
Pavel Moravec <pavelm@salsitasoft.com>
2015-10-08 10:27:14 +0200

    fix app component

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
-export class App extends Component {
+export default class App extends Component {
 
   render() {
     return (
